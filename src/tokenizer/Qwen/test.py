822897d926b3a037e4cd451c2e5eacd35d881d65
MLgdg <986285044@qq.com>
2024-01-09 20:27:49 +0800

    Qwen tokenizer

---
+++ src/tokenizer/Qwen/test.py
@@ -0,0 +1,10 @@
+
+from transformers import AutoTokenizer
+import torch 
+import os 
+
+
+tokenizer = AutoTokenizer.from_pretrained("./", trust_remote_code=True)
+
+inputs = tokenizer('蒙古国的首都是乌兰巴托（Ulaanbaatar）\n冰岛的首都是雷克雅未克（Reykjavik）\n埃塞俄比亚的首都是', return_tensors='pt')
+print (inputs)
